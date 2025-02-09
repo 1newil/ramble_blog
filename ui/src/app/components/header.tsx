3510ab0fcd676d857db34d0009450dddf4a461f7
GDS1221 <alexhong240@gmail.com>
2025-02-09 16:53:16 -0500

    fixed icon for dark mode

--- ui/src/app/components/header.tsx
+++ ui/src/app/components/header.tsx
@@ -10,9 +10,7 @@ export default function Header() {
       className={`mx-auto px-6 py-6 flex items-center justify-between transition-colors`}
     >
       <div className="flex items-center space-x-2">
-        <ChatBubbleOvalLeftEllipsisIcon
-          className={`w-8 h-8  dark:bg-white rounded-lg`}
-        />
+        <ChatBubbleOvalLeftEllipsisIcon className={`w-8 h-8 rounded-lg`} />
         <h1 className="text-2xl font-bold">Ramble</h1>
       </div>
       <div className="flex flex-row space-x-8 items-center">
